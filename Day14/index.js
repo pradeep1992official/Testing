b0b31b1ad84d1aa7982959ca75e69945b91f16d8
pradeep1992official <pradeep1992official@gmail.com>
2025-03-02 23:52:51 +0530

    design

--- Day14/index.js
+++ Day14/index.js
@@ -6,9 +6,10 @@ function urlOpen() {
   // var newWindow;
   if (url) {
     myWindow = window.open(url, "_blank");
-    appendAlert('Window Opened', 'success');
+    console.log(myWindow);
+    appendAlert("Window Opened", "success");
   } else {
-    appendAlert('Enter Email ID', 'success');
+    appendAlert("Enter Email ID", "success");
   }
 }
 
@@ -16,30 +17,29 @@ function urlClose() {
   if (myWindow) {
     myWindow.close();
     myWindow = null;
-    appendAlert('Window Closed', 'secondary');
+    appendAlert("Window Closed", "secondary");
   } else {
-    appendAlert('No Window is Open', 'primary');;
+    appendAlert("No Window is Open", "primary");
   }
 }
 
 function urlCheck() {
   if (myWindow && !myWindow.closed) {
-    appendAlert('The Window is still Open', 'warning');
+    appendAlert("The Window is still Open", "warning");
   } else {
-    appendAlert('The Window is Closed', 'danger');
+    appendAlert("The Window is Closed", "danger");
   }
 }
 
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
 const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
+  const wrapper = document.createElement("div");
   wrapper.innerHTML = [
     `<div class="alert alert-${type} alert-dismissible" role="alert">`,
     `   <div>${message}</div>`,
     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+    "</div>",
+  ].join("");
 
-  alertPlaceholder.append(wrapper)
-}
-
+  alertPlaceholder.append(wrapper);
+};
