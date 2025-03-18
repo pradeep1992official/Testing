dee4ad4306ea0389b75b3c8a5f0508c73357f6c9
pradeep1992official <pradeep1992official@gmail.com>
2025-03-19 00:52:24 +0530

    Day15

---
+++ Day15/index.js
@@ -0,0 +1,8 @@
+let w = window.innerWidth;
+let h = window.innerHeight;
+
+document.getElementById("demo").innerHTML = "width = " + w + " and height = " + h;
+
+function getScreenStatus() {
+    alert("width = " + w + " and height = " + h);
+}
